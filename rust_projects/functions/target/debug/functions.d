5272c5869ba0015e35ec0c7899c8b2377ce7857f
Henrique Gouveia <henrique.gouveia@zerum.com>
2018-02-09 18:15:59 -0200

    ADd functions and variables

---
+++ rust_projects/functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/Users/henriquegouveia/repo/rust/rust_projects/functions/target/debug/functions.dSYM: /Users/henriquegouveia/repo/rust/rust_projects/functions/src/main.rs
---
+++ rust_projects/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/henriquegouveia/repo/rust/rust_projects/variables/target/debug/variables.dSYM: /Users/henriquegouveia/repo/rust/rust_projects/variables/src/main.rs
