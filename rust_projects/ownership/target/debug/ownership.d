fa67e4593481a85365f910b488658cdeb4915927
Henrique Nascimento Gouveia <h.gouveia@icloud.com>
2018-02-11 00:02:12 -0200

    Add flow, ownership, struct

---
+++ rust_projects/ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/Users/henriquenascimentogouveia/repo/rust_projects/rust_projects/ownership/target/debug/ownership.dSYM: /Users/henriquenascimentogouveia/repo/rust_projects/rust_projects/ownership/src/main.rs
---
+++ rust_projects/struct_fn/target/debug/struct_fn.d
@@ -0,0 +1,1 @@
+/Users/henriquenascimentogouveia/repo/rust_projects/rust_projects/struct_fn/target/debug/struct_fn.dSYM: /Users/henriquenascimentogouveia/repo/rust_projects/rust_projects/struct_fn/src/main.rs
